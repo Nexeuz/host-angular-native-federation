8abe471ef35f813f29854c6f7423420374384e63
Nicolas Falla Rios <nicolas.falla@globant.com>
2024-12-11 23:47:09 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MySharedLibraryService } from 'my-shared-library';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'shell';
+
+  constructor(private sharedService: MySharedLibraryService) {
+    this.sharedService.addTodo('hola');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'remote-feature',
+    loadComponent: () =>
+      loadRemoteModule({
+        remoteName: 'remote',
+        exposedModule: './Component',
+      }).then((m) => m.AppComponent),
+  },
+];
